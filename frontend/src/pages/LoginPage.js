19ce395738ca431250824f9353639194ddfc3f31
AlandelFresno <alandelfresno2@gmail.com>
2022-04-26 15:58:45 -0300

    LoginPage and RegisterPage form
    Added in index.css background color and text color,
    Created  form with styles (pages.css and tailwind) of LoginPage and RegisterPage,

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './pages.css'
 
 const LoginPage = () => {
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>LoginPage</div>
+    <div className='w-screen'>
+      <div className='flex h-screen justify-center items-center '>
+        <form 
+          className=' flex flex-col items-center p-32 
+                      border-2 border-white border-opacity-100 border-solid 
+                      rounded-xl form_loginpage'
+          onSubmit={handleSubmit}
+        >
+        <div className='flex flex-col pb-4 pb-1'>
+          <label className='pb-2 text-lg'> Username </label>
+          <input className='rounded-lg pr-2 text-base pl-1'/>
+        </div>
+        <div className='flex flex-col pt-2 pb-1'>
+          <label className='pb-2 text-lg'> Password </label>
+          <input className='rounded-lg pr-2 text-base pl-1'/>
+        </div>
+        <button
+          className='page_button w-32 h-8 border-2 border-white rounded-lg mt-6 form_button'
+          
+        > Log In </button>
+        <Link to='/register' className='pt-8 text-blue-500'> Create Account </Link>
+        </form>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './pages.css';
 
 const RegisterPage = () => {
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>RegisterPage</div>
+    <div className='w-screen'>
+      <div className='flex h-screen justify-center items-center'>
+        <form 
+            className=' flex flex-col items-center p-32 
+                        border-2 border-white border-opacity-100 border-solid 
+                        rounded-xl form_registerpage'
+            onSubmit={handleSubmit}
+        >
+        <div className='flex flex-col pb-4'>
+          <label className='pb-2 text-lg'> Username</label>
+          <input className='rounded-lg pr-2 text-base pl-1'/>
+        </div>
+        <div className='flex flex-col pb-4'>
+          <label className='pb-2 text-lg'> Email </label>
+          <input className='rounded-lg pr-2 text-base pl-1'/>
+        </div>
+        <div className='flex flex-col pt-2 pb-1'>
+          <label className='pb-2 text-lg'> Password </label>
+          <input className='rounded-lg pr-2 text-base pl-1'/>
+        </div>
+        <div className='flex flex-col pt-2 pb-1'>
+          <label className='pb-2 text-lg'> Confirm Password </label>
+          <input className='rounded-lg pr-2 text-base pl-1'/>
+        </div>
+        <button
+          className='page_button w-32 h-8 border-2 border-white rounded-lg mt-6 form_button'
+          
+        > Register </button>
+        <Link to='/login' className='pt-8 text-blue-500'> Log in </Link>
+        </form>
+      </div>
+    </div>
   );
 };
 
