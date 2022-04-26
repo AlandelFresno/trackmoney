f78a2d956738cccf46d7739f7e42425cde2e2aa8
AlandelFresno <alandelfresno2@gmail.com>
2022-04-26 15:24:11 -0300

    Page creation
    Created HomePage, LoginPage and RegisterPage,
    Created pages.css and components.css,
    Update route's elements

--- frontend/src/routes/PrivateRoute.js
+++ frontend/src/routes/PrivateRoute.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
 
 const PrivateRoute = () => {
   return (
     <Routes>
-      <Route exact path='/' element={<></>} />
+      <Route exact path='/' element={<HomePage />} />
       <Route exact path='*' element={<Navigate to='/' />} />
     </Routes>
   );
