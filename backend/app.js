91a9a66f92498080bfc990f67e36e644c81537e2
AlandelFresno <alandelfresno2@gmail.com>
2022-05-03 14:48:41 -0300

    Database connection & users creation
    Created database connection (connection.js),
    Created userModel,
    handle creation & deletion of users

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const sequelize = require('./database/connection');
+const User = require('./models/userModel');
 // const & let
 const app = express();
 const port = process.env.PORT || 3001;
@@ -11,9 +13,42 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 dotenv.config({path: '.env.local'});
 
+// database
+
+User.sequelize.sync().then((req) => {
+    // Server Listen
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}...`);
+    });
+});
+
 // Routes
 app.get('/', (req, res) => {
     res.status(200).send('Hello world');
+});
+
+app.delete('/api/users/:id', (req, res) => {
+   User.destroy({
+       where: {
+           id : req.params.id
+       }
+   });
+   res.status(200).send('Deleted');
+});
+app.post('/api/users', (req, res) => {
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        createdat: Date.now()
+    })
+    .then( () => {
+        res.status(200).send('User created');
+    })
+    .catch((err) => {
+        res.send(err.message);
+    });
 });
 
 // app.get('/api/operation', (req, res) => {
@@ -24,8 +59,4 @@ app.get('/', (req, res) => {
 //     res.send(req.params.id);
 // });
 
-// Server Listen
-app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
-});
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+
+const Sequelize = require('sequelize');
+const sequelize = require('../database/connection');
+
+const User = sequelize.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    createdat: {
+        type: Sequelize.DATE,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+});
+
+module.exports = User;
