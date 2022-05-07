0c69f88805ef43ef1067982547cce6bf51cba958
AlandelFresno <alandelfresno2@gmail.com>
2022-05-07 10:45:24 -0300

    Users get request
    Creation of get request to /api/users. It returns the users with the exact name,
    Rearrange of post request

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,40 @@
 
 const express = require('express');
 const router = express.Router();
+const User = require('../models/userModel');
 
+router.get('/users', async(req, res) =>{
+    if ( req.body.name !== undefined) {
+        await User.findAll({
+            where:{
+                name: req.body.name
+            },
+            limit:10
+        }).then((users) => {
+            res.json(users);
+        }).catch((err) => {
+            console.log('Oops! something went wrong, : ', err);
+        });
+    } else {
+        res.status(400).send('Please enter a name to search');
+    };
+});
+
+router.post('/users', (req, res) => {
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        createdat: Date.now()
+    })
+    .then( () => {
+        res.status(200).send('User created');
+    })
+    .catch((err) => {
+        res.send(err.message);
+    });
+});
+    
 router.delete('/users/:id', (req, res) => {
     User.destroy({
         where: {
@@ -9,22 +42,6 @@ router.delete('/users/:id', (req, res) => {
         }
     });
     res.status(200).send('Deleted');
- });
+});
 
-
- router.post('/users', (req, res) => {
-     User.create({
-         name: req.body.name,
-         email: req.body.email,
-         password: req.body.password,
-         createdat: Date.now()
-     })
-     .then( () => {
-         res.status(200).send('User created');
-     })
-     .catch((err) => {
-         res.send(err.message);
-     });
- });
-
- module.exports = router;
+module.exports = router;
