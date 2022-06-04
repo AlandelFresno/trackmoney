8c5c878059de3be8d637264776848ff86bbfd073
AlandelFresno <alandelfresno2@gmail.com>
2022-06-04 16:44:00 -0300

    Fix: operation table deletes when server starts

--- backend/models/index.js
+++ backend/models/index.js
@@ -6,12 +6,13 @@ User.hasMany(Operation,{ onDelete: 'CASCADE', onUpdate:'CASCADE', foreignKey: {n
 Operation.belongsTo(User, {foreignKey: {name: 'userID', allowNull: false}});
 // Operation.belongsTo(User);
 
-db.authenticate().then(async() => {
-    // await User.sync({force: true});
-    await Operation.sync({force: true});
-}).catch ( err => {
-    console.log(err);
-});
+db.authenticate();
+// .then(async() => {
+//     // await User.sync({force: true});
+//     // await Operation.sync({force: true});
+// }).catch ( err => {
+//     console.log(err);
+// });
 
 module.exports = {
     User,
