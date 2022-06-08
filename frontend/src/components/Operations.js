594cea1ed99ee216b1ee3cab1b231c09dcc5b5af
AlandelFresno <alandelfresno2@gmail.com>
2022-06-08 18:44:41 -0300

    Edit & Delete Modal window
    Creation of Edit & Delete Modal window,
    Style for Modal windows,
    Handle  operations update and delete,

--- frontend/src/components/Operations.js
+++ frontend/src/components/Operations.js
@@ -9,23 +9,94 @@ const Operations = (params) => {
 
     const {amount, title, operationType, id} = params;
     
-    
-    // console.log(amount, title, operationType);
-    
+    console.log(title)
+
     const handleClickEdit = () => {
-        Swal.fire();
+        Swal.fire({
+            title: 'Edit the operation',
+            showCancelButton: true,
+            color: 'white',
+            background: '#1f1f1f',
+            confirmButtonColor: 'green',
+            cancelButtonColor: 'red',
+            html: 
+                `<label> Title </label>` +
+                `<input id='swal-input1' placeholder=${title} class='swal2-input customSwalBtn'/>` +
+                `<label> Amount </label>` +
+                `<input id='swal-input2' placeholder=${amount} class='swal2-input customSwalBtn'/>`,
+            input: 'select',
+            inputOptions: {
+                Expense: 'Expense',
+                Income: 'Income'
+            },
+            customClass: {
+                input: 'input_swal',
+            }
+        }).then((result) => {
+            if(result.isConfirmed){
+                let resultAmount;
+                let resultTitle;
+                
+                if ( document.getElementById('swal-input1').value === ''){
+                    resultTitle = title;
+                } else {
+                    resultTitle = document.getElementById('swal-input1').value;
+                };
+                if ( document.getElementById('swal-input2').value === ''){
+                    resultAmount = amount;
+                } else {
+                    resultAmount = document.getElementById('swal-input2').value;
+                };
+                axios.put(`http://localhost:3001/api/operation`, {
+                    id,
+                    title: resultTitle,
+                    amount: resultAmount,
+                    operationType: result.value
+                }).then().catch(err => console.log(err));
+                Swal.fire({
+                    title: 'Operation updated',
+                    icon: 'success',
+                    color: 'white',
+                    confirmButtonColor: 'green',
+                    iconColor: 'green',
+                    background: '#1f1f1f',
+                    timer: 3000,
+                }).then(() => {document.location.reload()});
+            };
+        }).catch(err => console.log(err));
     };
     
     const handleClickDelete = () => {
-        
-        // console.log(id)
-
-        axios.delete('http://localhost:3001/api/operation', {
-            params: {
-                id
-            }
-        });
-
+        Swal.fire({
+            title: 'Want to delete it?',
+            color: 'white',
+            icon: 'warning',
+            iconColor: 'white',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it',
+            confirmButtonColor: 'green',
+            cancelButtonText: 'No, cancel',
+            cancelButtonColor: 'red',
+            background: '#1f1f1f'
+        }).then ((ressult) => {
+                if(ressult.isConfirmed) {
+                    axios.delete('http://localhost:3001/api/operation', {
+                        params: {
+                            id
+                        }
+                    });
+                    Swal.fire({
+                        title: 'Operation deleted',
+                        icon: 'success',
+                        color: 'white',
+                        confirmButtonColor: 'green',
+                        iconColor: 'green',
+                        background: '#1f1f1f',
+                        timer: 2000,
+                    }).then(() => {document.location.reload()});
+                };
+            })
+          .catch(err => console.log(err));
     };
 
   return (
@@ -33,9 +104,9 @@ const Operations = (params) => {
         <h5 className='title_width'>{title}</h5>
         {
             operationType === 'Income' ? 
-                <p className='text-green-700'> {amount} </p>  
+                <p className='text-green-700'> ${amount} </p>  
             :
-                <p className='text-red-600'> {amount} </p>
+                <p className='text-red-600'> ${amount} </p>
         }
         <div className='title_width flex justify-end'>
             <EditIcon onClick={handleClickEdit} className='cursor-pointer' fontSize='small'/>
