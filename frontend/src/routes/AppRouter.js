69025cf95b7f837acde1a793e2fb503a9f47ed0b
AlandelFresno <alandelfresno2@gmail.com>
2022-06-08 18:36:01 -0300

    Fix: on Reload auth fails
    on reload auth failed,
    dispatch LogIn and NameLogIn In AppRouter to save the localStorage data.

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
+import { LogIn } from '../redux/actions/auth';
+import { NameLogIn } from '../redux/actions/name';
 
 const AppRouter = () => {
 
+  const dispatch = useDispatch();
   let token = false;
   const tokenRedux = useSelector(state => state.auth.login);
   const tokenStore = (localStorage.getItem('auth') === 'true');
+  const nameStore = localStorage.getItem('name');
 
+
+  if (tokenStore) {
+    dispatch(LogIn());
+  };
+  if( nameStore !== '') {
+    dispatch(NameLogIn(nameStore));
+  };
   if (tokenRedux || tokenStore) {
     token = true;
   };
