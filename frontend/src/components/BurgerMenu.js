51b747641f609a5085f777923b69b486f771b868
AlandelFresno <alandelfresno2@gmail.com>
2022-06-14 18:44:33 -0300

    BurgerMenu Style
    Improvement of BurgerMenu style,
    Implemented background blur for sweetalert

--- frontend/src/components/BurgerMenu.js
+++ frontend/src/components/BurgerMenu.js
@@ -25,7 +25,7 @@ const BurgerMenu = () => {
           !isOpen ? 
             (
               <button 
-                className='fixed z-30 flex items-center cursor-pointer right-4 top-6 bg-transparent border-0 '
+                className='fixed z-30 flex items-center cursor-pointer right-8 top-6 bg-transparent border-0 '
                 onClick={() => setIsOpen(!isOpen)}
               >
                 <svg
@@ -34,27 +34,27 @@ const BurgerMenu = () => {
                   width="40"
                   height="35"
                 >
-                  <rect y="0" width="60" height="4" fill='white'></rect>
-                  <rect y="20" width="60" height="4" fill='white'></rect>
-                  <rect y="40" width="60" height="4" fill='white'></rect>
+                  <rect y="0" width="70" height="5" fill='white'></rect>
+                  <rect y="20" width="70" height="5" fill='white'></rect>
+                  <rect y="40" width="70" height="5" fill='white'></rect>
                 </svg>
               </button>
             ) 
           : 
             (
               <button 
-                className='text-xl z-30 fixed top-6 right-10 cursor-pointer bg-transparent border-0 text-white'
+                className='text-xl z-30 fixed top-6 right-14 cursor-pointer bg-transparent border-0 text-white'
                 onClick={() => setIsOpen(!isOpen)}
               > X </button>
             )
         }
-        <div className={`top-0 right-0 fixed ${isOpen ? 'translate-x-0' : 'translate-x-full'} ease-in-out duration-300`}>    
-          <div className='mt-16 mr-8 flex flex-col'>
-            <Link to='/home' className='no-underline text-white pb-2'> Home </Link>
-            <Link to='/operations' className='no-underline text-white pb-2'> Records </Link>
-            <Link to='/about' className='no-underline text-white pb-2'> About </Link>
+        <div className={`top-0 right-0 fixed ${isOpen ? 'translate-x-0 bg_burgerMenu h-screen pl-4' : 'translate-x-full h-screen'} ease-in-out duration-300`}>    
+          <div className='mt-20 mr-8 flex flex-col'>
+            <Link to='/home' className='no-underline text-white pb-2 link_burger '> Home </Link>
+            <Link to='/operations' className='no-underline text-white pb-2 link_burger'> Records </Link>
+            <Link to='/about' className='no-underline text-white pb-2 link_burger'> About </Link>
           </div>
-            <button className='mt-4' onClick={handleClick}> logout </button>
+            <button className='mt-4 w-24 logoutbutton' onClick={handleClick}> Logout </button>
         </div>
       </div>
   )
